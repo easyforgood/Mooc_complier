6cecf4e6b537bae1a484761b9463abc4b807c532
Sip <easyforgood@hotmail.com>
2015-05-26 23:44:09 +0800

    hw2
    hw2

--- src/complier_bjhua_hw2/main.c
+++ src/complier_bjhua_hw2/main.c
@@ -1,20 +1,38 @@
+/**
+ * homework2
+ * 课程总结、代码解释和效果截图参见：
+ * https://github.com/easyforgood/Mooc_complier/blob/master/src/complier_bjhua_hw2/README.md
+ * 或者项目主页的介绍
+ * 
+ **/
+
 #include<stdio.h>
 #include<stdlib.h>
 #define FILENAME "testfile"
-
-FILE *fp;
-int line=1;
-int linePos=1;
-int startPos=0;
-int nowPos=0;
-//0 1 2 3 4 
+//需要的全局变量
+FILE *fp; //文件指针
+int line=1; //对应行号
+int linePos=1; //对应行列号
+int startPos=0; //上一个词的起点位置
+int nowPos=0; //文件指针现在的位置 = ftell(fp)
+/*
+用作状态转移的参数
+0 1 2 3 4这几种状态
+0 代表无任何输入的状态，非最终状态
+1 第一次输入i的状态，非最终状态
+2 第一次输入为数字的状态，最终状态，代表输入的是NUM，第一次输入为数字，说明只能为数字或者异常输入
+3 第二次输入为f,临界状态，可能为非最终，也可能为最终，代表输入为IF。这里偷懒了。
+4 输入为ID，最终状态。
+*/ 
 int state=0;
+//print IF
 void printIF(){
     printf("IF(%d,%d)\n",line,linePos);
     nowPos=ftell(fp);
     startPos=nowPos;
     state=0;
 }
+//print ID
 void printID(){
     int tempPos=ftell(fp);
     fseek(fp,startPos-tempPos,SEEK_CUR);
@@ -25,18 +43,19 @@ void printID(){
     startPos=nowPos;
     state=0;
 }
+//print NUM
 void printINT(){
      int tempPos=ftell(fp);
     fseek(fp,startPos-tempPos,SEEK_CUR);
     char *s=(char *)malloc(sizeof(char)*(nowPos-startPos));
     fread(s,sizeof(char),nowPos-startPos,fp);     
-    printf("INT(%s)(%d,%d)\n",s,line,linePos);
+    printf("NUM(%s)(%d,%d)\n",s,line,linePos);
     nowPos=ftell(fp);
     startPos=nowPos;
     state=0;
 }
 
-
+//词法分析
 void lexer(int ch){
     switch(state){
         case 0:
@@ -85,11 +104,14 @@ void lexer(int ch){
         default:break;
 
     }
+    //每次操作都对齐nowPos
     nowPos=ftell(fp);
 
 }
 void main(int argc,char *argv[]) //命令行参数
 {
+    //默认文件名为testfile
+    //文件操作参考了网上的，见：testFile.c文件
     int ch;
     char *filename=FILENAME;
     if(argc==2)
@@ -103,6 +125,7 @@ void main(int argc,char *argv[]) //命令行参数
         return;
     }
     int state;
+    //列号，行行号记录
     nowPos=startPos=ftell(fp);
     ch=fgetc(fp); 
     while(ch!=EOF) 
